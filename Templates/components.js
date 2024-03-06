8fa570f80c3eb47693904de81c8edb4edd686269
staytheknight <staytheknight@gmail.com>
2024-03-05 21:17:18 -0700

    Bringing in Sumerah's alt footer for the search screen

--- Templates/components.js
+++ Templates/components.js
@@ -28,6 +28,23 @@ class Footer extends HTMLElement {
     }
 }
 
+class Footer2 extends HTMLElement {
+    connectedCallback(){
+        this.innerHTML = `
+            <div class="footer">
+                <button class="back">
+                    <img class="offset-img" src="../Images/Icons/Screens/back red.png" alt="Back arrow" />
+                    Back
+                </button>
+                <button class="confirm">
+                    Search
+                    <img class="offset-img" src="../Images/Icons/Screens/search green.png" alt="Green search icon" />
+                </button>
+            </div>
+        `
+    }
+}
+
 class TripItem extends HTMLElement {
     
     connectedCallback() {
@@ -36,9 +53,6 @@ class TripItem extends HTMLElement {
             <div class='single-item'>
                 <label id='item-label' for="label"></label>
                 <input id='item-input' type="radio" name="label" >
-            </div>
-            <div>
-
             </div>
         </div>
         `;
@@ -64,3 +78,4 @@ class TripItem extends HTMLElement {
 customElements.define('main-header', Header);
 customElements.define('main-footer', Footer);
 customElements.define('trip-item', TripItem);
+customElements.define('alt-footer',Footer2);
