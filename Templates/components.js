fad74a7f2925fd1215870563cb8cc922abc2c2fb
Elise Chevalier <staytheknight@gmail.com>
2024-03-31 17:15:53 -0600

    Created drop down menu for origin and destination
    The menu won't close yet, and a selection can't be made when clicking on an drop down option

--- Templates/components.js
+++ Templates/components.js
@@ -383,6 +383,21 @@ function indexToPath(index) {
     return url;
 }
 
+// filter function for drop down menus
+function filterFunction(input, div, a) {
+    var filter = input.value.toUpperCase();
+
+    for (var i=0; i<a.length; i++) {
+      txtValue = a[i].textContent || a[i].innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        a[i].style.display = "";
+      } else {
+        a[i].style.display = "none";
+      }
+    }
+    div.style.display = "block";
+  }
+
 // waits for the page to be fully loaded before buttons can be pressed
 document.addEventListener("DOMContentLoaded", function() {
     backButtonClick();
