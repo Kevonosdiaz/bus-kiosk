978e51c7c40c6d6c5bb0cd57ea71008dcae95a6f
justinclibbett <127788023+justinclibbett@users.noreply.github.com>
2024-04-06 17:32:40 -0600

    working counter for remaining seats

--- a4_seat_selector/seats.js
+++ a4_seat_selector/seats.js
@@ -1,9 +1,23 @@
 var checks = document.querySelectorAll('input[type=checkbox]');
-var max = 2;
+console.log(sessionStorage.getItem("passengers"))
+var max = Number(sessionStorage.getItem("passengers"));
+
 for (var i = 0; i < checks.length; i++)
   checks[i].onclick = selectiveCheck;
+
 function selectiveCheck (event) {
+  console.log("click")
+  
   var checkedChecks = document.querySelectorAll(".seat:checked");
+  updateText(checkedChecks.length);
   if (checkedChecks.length >= max + 1)
     return false;
 }
+
+function updateText(checked)  {
+  var rem = String(max - checked);
+  if (rem < 0)
+    rem = 0;
+  document.getElementById('count').innerText = "Remaining: " + rem + " / " + String(max);
+}
+updateText(0);
