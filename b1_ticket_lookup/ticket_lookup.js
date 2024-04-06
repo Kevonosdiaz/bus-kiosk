29d7429cdefcacd4c6da5c173581604e6141d9d8
justinclibbett <127788023+justinclibbett@users.noreply.github.com>
2024-04-06 17:34:04 -0600

    changed verification process

--- b1_ticket_lookup/ticket_lookup.js
+++ b1_ticket_lookup/ticket_lookup.js
@@ -1,5 +1,27 @@
-$(document).ready(function(){
-    $(".verify_label").click(function(){
-      $("#verification-box").toggle();
-    });
+const verifyLabel = document.getElementById('verifyLabel');
+console.log(verifyLabel)
+function verifyInfo() {
+  verifyLabel.addEventListener('click', function() {
+    var div = document.getElementById('vbox');
+    if (div.style.display === "flex") {
+        div.style.display = "none";
+    } else {
+        div.style.display = "flex";
+        verifyLabel.style.display = 'none';
+    }
   });
+}
+
+document.getElementById('next').onclick = function () {
+    location.href = "../b3_modify_ticket_new/modify_1.html";
+};
+
+document.getElementById('back').onclick = function () {
+    location.href = "../index.html"
+};
+
+document.addEventListener("DOMContentLoaded", function() {
+    verifyInfo();
+});
+
+
