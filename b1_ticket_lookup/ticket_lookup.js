0e18f2f1e1e7bf6aaf0413e1fb59616e37cb1259
justinclibbett <127788023+justinclibbett@users.noreply.github.com>
2024-04-07 18:39:05 -0600

    required to enter info into fields to click verify

--- b1_ticket_lookup/ticket_lookup.js
+++ b1_ticket_lookup/ticket_lookup.js
@@ -1,13 +1,52 @@
 const verifyLabel = document.getElementById('verifyLabel');
-console.log(verifyLabel)
+
+sessionStorage.setItem('name', "")
+sessionStorage.setItem('email', "")
+sessionStorage.setItem('phone', "")
+sessionStorage.setItem('bookingid', "")
+function handleInput(fieldName, value) {
+    switch (fieldName) {
+      case 'name':
+      case 'email':
+      case 'phone':
+      case 'bookingid':
+        sessionStorage.setItem(fieldName, value)
+        if (!checkEmpty()) {
+            verifyLabel.style.backgroundColor = "#7197FF";
+            verifyLabel.style.color = "#000000";
+            verifyLabel.style.cursor = 'pointer';
+        } else {
+            verifyLabel.style.cursor = 'not-allowed';
+            verifyLabel.style.backgroundColor = "#C6C6C6";
+            verifyLabel.style.color = "#5D5D5D";
+        }
+       /* if (value === '') {
+          showRequiredTag(fieldName)
+        } else {
+          hideRequiredTag(fieldName)
+        }
+        validateFormState()*/
+    }
+  }
+
+function checkEmpty() {
+    if (sessionStorage.getItem('name') == "" || sessionStorage.getItem('email') == "" || sessionStorage.getItem('phone') == "" || sessionStorage.getItem('bookingid') == "") {
+        return true
+    } else {
+        return false
+    }
+}
+
 function verifyInfo() {
   verifyLabel.addEventListener('click', function() {
     var div = document.getElementById('vbox');
-    if (div.style.display === "flex") {
-        div.style.display = "none";
-    } else {
-        div.style.display = "flex";
-        verifyLabel.style.display = 'none';
+    if (!checkEmpty()) {
+        if (div.style.display === "flex") {
+            div.style.display = "none";
+        } else {
+            div.style.display = "flex";
+            verifyLabel.style.display = 'none';
+        }
     }
   });
 }
