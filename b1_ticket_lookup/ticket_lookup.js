c36af8244502849a9d4bba32079654facaa5a818
justinclibbett <127788023+justinclibbett@users.noreply.github.com>
2024-04-07 20:29:40 -0600

    now requires a valid booking input

--- b1_ticket_lookup/ticket_lookup.js
+++ b1_ticket_lookup/ticket_lookup.js
@@ -1,6 +1,8 @@
 const verifyLabel = document.getElementById('verifyLabel');
 const nextBtn = document.getElementById('next');
 
+const valid_inputs = ["gus ryder", "gus.ryder@bmail.com", "1112223333", "bus123", "737373"]
+
 sessionStorage.setItem('m-name', "")
 sessionStorage.setItem('m-email', "")
 sessionStorage.setItem('m-phone', "")
@@ -24,7 +26,7 @@ function handleInput(fieldName, value) {
         }
       case 'm-vcode':
         sessionStorage.setItem(fieldName, value)
-        if (verifyCode()) {
+        if (checkFormatCode()) {
             nextBtn.style.backgroundColor = "#7197FF";
             nextBtn.style.color = "#000000";
             nextBtn.style.cursor = 'pointer';
@@ -52,8 +54,30 @@ function checkFormat() {
     }
 }
 
+function findBooking () {
+    var name = sessionStorage.getItem('m-name').toLowerCase();
+    var email = sessionStorage.getItem('m-email').toLowerCase();
+    var phone = sessionStorage.getItem('m-phone').toLowerCase();
+    var id = sessionStorage.getItem('m-bookingid').toLowerCase();
+    if (name == valid_inputs[0] && email == valid_inputs[1] && phone == valid_inputs[2] && id == valid_inputs[3]) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function checkFormatCode() {
+    var vcode = sessionStorage.getItem('m-vcode');
+    if ( /^\d{6}$/.test(vcode)) {
+        return true
+    } else {
+        return false
+    }
+}
+
 function verifyCode() {
-    if ( /^\d{6}$/.test(sessionStorage.getItem('m-vcode'))) {
+    var vcode = sessionStorage.getItem('m-vcode');
+    if (vcode == valid_inputs[4]) {
         return true
     } else {
         return false
@@ -63,14 +87,23 @@ function verifyCode() {
 function verifyInfo() {
   verifyLabel.addEventListener('click', function() {
     var div = document.getElementById('vbox');
+    var message = document.getElementById('message_box');
+    var msgtext = document.getElementById('msg');
     if (!checkEmpty() && checkFormat()) {
-        if (div.style.display === "flex") {
-            div.style.display = "none";
+        if (findBooking()) {
+            if (div.style.display === "flex") {
+                div.style.display = "none";
+            } else {
+                message.style.display = "none";
+                div.style.display = "flex";
+                document.getElementById('vbox_text').innerText = "Please enter the verification code sent to \n" + String(sessionStorage.getItem('m-email'));
+                verifyLabel.style.display = 'none';
+            }
         } else {
-            div.style.display = "flex";
-            document.getElementById('vbox_text').innerText = "Please enter the verification code sent to \n" + String(sessionStorage.getItem('m-email'));
-            verifyLabel.style.display = 'none';
+            message.style.display = "flex";
+            msgtext.innerText = "No booking matches this information. Please check that you have entered the correct information and try again.";
         }
+        
     }
   });
 }
@@ -78,6 +111,24 @@ function verifyInfo() {
 nextBtn.onclick = function () {
     if (verifyCode()) {
         location.href = "../b2_modify_stream/b2.1_modify_booking_options.html";
+    } else {
+        var div = document.getElementById('vbox');
+        var message = document.getElementById('message_box');
+        var msgtext = document.getElementById('msg');
+
+        var code_input = document.getElementById('code-input');
+        code_input.value = "";
+
+        sessionStorage.setItem('m-vcode', "");
+
+        verifyLabel.style.display = 'flex';
+        div.style.display = "none";
+        message.style.display = "flex";
+        msgtext.innerText = "Verification code was incorrect. Please submit again to receive a new code.";
+
+        nextBtn.style.cursor = 'not-allowed';
+        nextBtn.style.backgroundColor = "#C6C6C6";
+        nextBtn.style.color = "#5D5D5D";
     }
     
 };
